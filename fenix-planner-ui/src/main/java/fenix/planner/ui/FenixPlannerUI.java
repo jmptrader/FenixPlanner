585e06600f8890b6c5bb23ecce34c0925a659c2f
Petter Holmstroem <petter@vaadin.com>
2013-12-15 00:29:16 +0200

    Added license header and manifest

--- fenix-planner-ui/src/main/java/fenix/planner/ui/FenixPlannerUI.java
+++ fenix-planner-ui/src/main/java/fenix/planner/ui/FenixPlannerUI.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package main.java.fenix.planner.ui;
+package fenix.planner.ui;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
